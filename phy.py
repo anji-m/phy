07cb2d7cfe721862bdf3b63cda427768da804178
luc <luc@sietium.com>
2021-12-08 21:54:47 +0800

    almost complete
    Signed-off-by: luc <luc@sietium.com>

--- phy.py
+++ phy.py
@@ -1,47 +1,50 @@
 #!/usr/bin/env python3
 
 from mpl_toolkits.axes_grid1 import host_subplot
+from matplotlib.pyplot import figure
 import matplotlib.pyplot as plt
 import numpy as np
+
+figure(figsize=(19, 14), dpi=80)
 
 # one row, one column, plot number 1
 host = host_subplot(111)
 para = host.twinx()
 
-host.set_xlabel("Months")
-host.set_ylabel("Weight")
-para.set_ylabel("Height")
-para.set_yticks([10, 20, 30, 40, 50, 60, 70])
+font = {
+    'weight': 'bold',
+    'size': 18,
+}
+
+host.set_xlabel("Months", fontdict=font)
+host.set_ylabel("Weight", fontdict=font)
+para.set_ylabel("Height", fontdict=font)
 
 months = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
 weight_s = np.array([3.5, 4.1, 4.7, 5.3, 5.9, 6.4, 6.9, 7.2, 7.6, 7.9, 8.1])
+weight_l = np.array([6.8, 7.7, 8.5, 9.2, 9.8, 10.3, 10.8, 11.3, 11.7, 12.0, 12.4])
+
 height_s = np.array([52.9, 55.8, 58.3, 60.5, 62.4, 64.1, 65.7, 67.0, 68.3, 69.6, 70.7])
+height_l = np.array([63.2, 66.4, 69.1, 71.3, 73.2, 74.8, 76.3, 77.3, 78.9, 80.2, 81.5])
 
 with plt.style.context('fivethirtyeight'):
-    p1, = host.plot(months, weight_s, label="Weight")
-    p2, = para.plot(months, height_s, label="Height")
+    pw_s, = host.plot(months, weight_s, color="#ff8c00", label="Weight")
+    pw_l, = host.plot(months, weight_l, color="#ff8c00")
+    ph_s, = para.plot(months, height_s, color="#436eee", label="Height")
+    ph_l, = para.plot(months, height_l, color="#436eee")
     legd = plt.legend()
 
-plt.subplots_adjust(hspace=1.2)
+host.yaxis.get_label().set_color(pw_s.get_color())
+host.set_xticks([2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+host.set_yticks([3., 3.5, 4., 4.5, 5., 5.5, 6., 6.5, 7., 7.5,
+                 8., 8.5, 9., 9.5, 10., 10.5, 11., 11.5, 12., 12.5])
+legd.texts[0].set_color(pw_s.get_color())
 
-host.yaxis.get_label().set_color(p1.get_color())
-legd.texts[0].set_color(p1.get_color())
+para.yaxis.get_label().set_color(ph_s.get_color())
+para.set_yticks([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85])
+legd.texts[1].set_color(ph_s.get_color())
 
-para.yaxis.get_label().set_color(p2.get_color())
-legd.texts[1].set_color(p2.get_color())
-
-large_boy = np.array([
-    (6.8, 63.2),
-    (7.7, 66.4),
-    (8.5, 69.1),
-    (9.2, 71.3),
-    (9.8, 73.2),
-    (10.3, 74.8),
-    (10.8, 76.3),
-    (11.3, 77.3),
-    (11.7, 78.9),
-    (12.0, 80.2),
-    (12.4, 81.5)
-], dtype=[('weight', 'f4'), ('height', 'f4')])
+plt.title("Anji's Physical Growth", fontdict={'fontsize': 24, 'fontweight': 'bold'})
 
 plt.savefig('phy.png')
